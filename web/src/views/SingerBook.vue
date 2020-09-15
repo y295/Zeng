c0ed5131b1ecb8f8438f606e42b02d61a19dca1e
y295 <55754990+y295@users.noreply.github.com>
2020-09-15 20:05:02 +0800

    Update SingerBook.vue

--- web/src/views/SingerBook.vue
+++ web/src/views/SingerBook.vue
@@ -3,61 +3,59 @@
     <div class="title"><span><b>歌手库</b></span></div>
     <div class="singer_list">
       <ul>
-          <li v-for="item in singer_list" >
-              <span class="singerR_name"><a @click="common.singer(item._id)">{{item.singer_name}}</a>
-              </span>
-          </li>
+        <li v-for="item in singer_list" >
+          <span class="singerR_name"><a @click="common.singer(item._id)">{{item.singer_name}}</a></span>
+        </li>
       </ul>
     </div>
     <div class="block">
       <el-pagination
-      layout="prev, pager, next"
-      :page-count="pagination.totalPage"
-      :current-page="pagination.currentPage"
-      @current-change="handleCurrentChange">
+        layout="prev, pager, next"
+        :page-count="pagination.totalPage"
+        :current-page="pagination.currentPage"
+        @current-change="handleCurrentChange">
       </el-pagination>
     </div>
   </div>
 </template>
 
 <style>
-  .SingerBook_wrap{
-     border: 0;
-     overflow: hidden;
-     clear: both;
-     background-color: #fafafa;
-     padding: 0;
-     margin: 0;
-     height: 100%;
+.SingerBook_wrap{
+   border: 0;
+   overflow: hidden;
+   clear: both;
+   background-color: #fafafa;
+   padding: 0;
+   margin: 0;
+   height: 100%;
 }
-  .title{
-    clear: both;
-    font-size: 18px;
-    color: #2a2a2a;
-    margin-left: 10px;
+.title{
+  clear: both;
+  font-size: 18px;
+  color: #2a2a2a;
+  margin-left: 10px;
 }
-
-  .block{
-    text-align: center;
-    margin: 15px 0;
-  }
-  .el-table{
-    margin-left: 10px;
-    margin-right: 10px
+.block{
+  text-align: center;
+  margin: 15px 0;
 }
-  ul {
-    list-style: none;
-    display: block;
+.el-table{
+  margin-left: 10px;
+  margin-right: 10px
 }
-  .SingerBook_wrap .singer_list li {
-    overflow: hidden;
-    display: block;
-    float: left;
-    margin-left: 10px;
-    padding-bottom: 10px;
-    padding-top: 10px;
-    width: 100px;
- }
+ul {
+  list-style: none;
+  display: block;
+}
+.SingerBook_wrap .singer_list li {
+  overflow: hidden;
+  display: block;
+  float: left;
+  margin-left: 10px;
+  padding-bottom: 10px;
+  padding-top: 10px;
+  width: 100px;
+}
 </style>
 
 <script>
@@ -73,15 +71,14 @@ export default {
   },
  methods: {
     async fetchSingerList() {
-        const res = await this.$http.get(`/musics/singerbook/${this.pagination.currentPage}`+'?data=' + new Date().getTime());
-        this.singer_list = res.data.list;
-        this.pagination.totalPage = res.data.totalPage;
+      const res = await this.$http.get(`/musics/singerbook/${this.pagination.currentPage}`+'?data=' + new Date().getTime());
+      this.singer_list = res.data.list;
+      this.pagination.totalPage = res.data.totalPage;
     },
     handleCurrentChange (currentPage) {
       this.pagination.currentPage = currentPage
       this.fetchSingerList();
     }
-
  },
 created() {
   this.fetchSingerList()
