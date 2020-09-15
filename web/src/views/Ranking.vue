6ef480f1e9cae02212a2424842ad1c46e6f72da5
y295 <55754990+y295@users.noreply.github.com>
2020-09-15 18:55:40 +0800

    Update Ranking.vue

--- web/src/views/Ranking.vue
+++ web/src/views/Ranking.vue
@@ -1,39 +1,40 @@
 <template>
   <div class="ranking_wrap">
-  <div class="title"><span><b>音乐排行榜</b></span></div>
-  <div class="music_list">
+    <div class="title"><span><b>音乐排行榜</b></span></div>
+    <div class="music_list">
       <el-table
-      :data="music_list"
-      style="width: 100%"
-      max-height="420"
-      :header-cell-style="{background:'#fafafa'}"
-      :row-style="{height:0+'px', background:'#fafafa'}"
-      :cell-style="{padding:1+'px'}">
+        :data="music_list"
+        style="width: 100%"
+        max-height="420"
+        :header-cell-style="{background:'#fafafa'}"
+        :row-style="{height:0+'px', background:'#fafafa'}"
+        :cell-style="{padding:1+'px'}">
         <el-table-column  label="排名" type="index" width="50">
         </el-table-column>
         <el-table-column prop="music_name" label="音乐名" type="index" width="150">
-        <template slot-scope="scope">
-          <a @click="common.setNowMusic(scope.row)">{{scope.row.music_name}}</a>
-        </template>
+          <template slot-scope="scope">
+            <a @click="common.setNowMusic(scope.row)">{{scope.row.music_name}}</a>
+          </template>
         </el-table-column>
-      <el-table-column
-      prop="singers_list"
-      label="歌手" >
-      <template slot-scope="scope">
-        <li v-for="item in scope.row.singers_list" @click="common.singer(item._id)">{{item.singer_name}}</li>
-      </template>
-    </el-table-column>
-    <el-table-column
-      prop="categories"
-      label="类型" >
-      <template slot-scope="scope">
-        <li v-for="item in scope.row.categories" @click="() => $router.push(`/musics/musicbook/${item._id}`)">{{item.title}}</li>
-      </template>
-    </el-table-column >
-    <el-table-column  label="收藏量" prop="collection_num">
-    </el-table-column>
-  </el-table>
-  </div>
+        <el-table-column
+          prop="singers_list"
+          label="歌手" >
+          <template slot-scope="scope">
+            <li v-for="item in scope.row.singers_list" @click="common.singer(item._id)">{{item.singer_name}}</li>
+          </template>
+        </el-table-column>
+        <el-table-column
+          prop="categories"
+          label="类型" >
+          <template slot-scope="scope">
+            <li v-for="item in scope.row.categories" @click="() => $router.push(`/musics/musicbook/${item._id}`)">
+            {{item.title}}</li>
+          </template>
+        </el-table-column >
+        <el-table-column  label="收藏量" prop="collection_num">
+        </el-table-column>
+      </el-table>
+    </div>
   </div>
 </template>
 
@@ -54,16 +55,12 @@
   background-color: #fafafa;
   margin: 0
 }
-
 .el-table{
     margin-bottom: 20px
 }
-
 .el-table .cell{
   cursor: pointer;
-
 }
-
 .title{
   clear: both;
   font-size: 18px;
